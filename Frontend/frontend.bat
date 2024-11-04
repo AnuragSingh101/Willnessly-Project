6b0c54b1184f1f28e865541fca6d15dc5f0c2e02
Anurag Singh Rajput <anus82101@gmail.com>
2024-11-04 06:01:26 +0530

    added an auto intallation of npm pacakages and dependencies

---
+++ Frontend/frontend.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing front-end dependencies...
+npm install axios body-parser cors express react react-dom react-router-dom switch
+
+echo Installation complete.
+pause
+
---
+++ backened/backend.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Installing front-end dependencies...
+npm install nodemon bcryptjs bcrypt cors dotenv express jsonwebtoken mongoose
+echo Installation complete.
+pause
+
